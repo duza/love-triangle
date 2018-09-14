4eb7eb3f876fd5ff3d2c22d9088cce7742ff20cc
duza <duza@tut.by>
2018-09-15 01:32:40 +0300

    Try to solve a task
    We iterate through array and for each i-th element we go on apexes
    and if they are different and end apex equals to start and number equalt
    to 3 we add to count of triangles.
    
    Relates #1

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  preferences.forEach((value, lover) => {
+    let whoHeLove = value - 1; 
+    let i = 0;
+    const apexesOfTriangle = [whoHeLove];
+    while (i < 2) {
+      const nextLover = preferences[whoHeLove] - 1;
+      if (!apexesOfTriangle.includes(nextLover)) {
+        apexesOfTriangle.push(nextLover);
+        whoHeLove = nextLover;
+      }
+      i++;
+    }
+    if (apexesOfTriangle.length === 3 && whoHeLove === value - 1) {
+      count++
+    }
+  });
+  return count;
 };
